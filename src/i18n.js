44b170bebf9f29e5cfc4fa146aed93f1df784e1d
Konstantin.L <llaskot@gmail.com>
2026-06-07 18:08:29 +0300

    v2

--- src/i18n.js
+++ src/i18n.js
@@ -1,16 +1,36 @@
+// import i18n from "i18next";
+// import { initReactI18next } from "react-i18next";
+// import en from "./locales/en.json";
+// import uk from "./locales/uk.json";
+//
+// i18n.use(initReactI18next).init({
+//     resources: {
+//         en: { translation: en },
+//         ua: { translation: uk }
+//     },
+//     lng: localStorage.getItem("lang") || "en",
+//     fallbackLng: "en",
+//     interpolation: { escapeValue: false }
+// });
+//
+// export default i18n;
+
+
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import en from "./locales/en.json";
-import uk from "./locales/uk.json";
+import HttpBackend from "i18next-http-backend";
 
-i18n.use(initReactI18next).init({
-    resources: {
-        en: { translation: en },
-        ua: { translation: uk }
-    },
-    lng: localStorage.getItem("lang") || "en",
-    fallbackLng: "en",
-    interpolation: { escapeValue: false }
-});
+i18n
+    .use(HttpBackend) // Подключаем загрузку по HTTP
+    .use(initReactI18next)
+    .init({
+        lng: localStorage.getItem("lang") || "en",
+        fallbackLng: "en",
+        interpolation: { escapeValue: false },
+        backend: {
+            // Путь, откуда брать файлы. Обязательно с /Dev_CV/, так как это базовый путь на GitHub Pages
+            loadPath: '/Dev_CV/locales/{{lng}}/translation.json'
+        }
+    });
 
 export default i18n;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,28 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// import "./i18n";
+//
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//     {/*  ReactDOM.createRoot(document.getElementById("root")).render(<App />);*/}
+//   </StrictMode>,
+// )
+
+
+import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import "./i18n";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-    {/*  ReactDOM.createRoot(document.getElementById("root")).render(<App />);*/}
-  </StrictMode>,
+    <StrictMode>
+        {/* Suspense покажет текст "Загрузка...", пока JSON-файл скачивается */}
+        <Suspense fallback={<div>Loading...</div>}>
+            <App />
+        </Suspense>
+    </StrictMode>,
 )
